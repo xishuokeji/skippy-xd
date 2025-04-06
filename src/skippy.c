c642077e6af28098e6eccb3898d480bffd5df8b5
Felix Fung <fylixeoi@gmail.com>
2025-04-06 01:31:34 -0700

    For paging, have tooltips on top of desktops

--- src/skippy.c
+++ src/skippy.c
@@ -1138,13 +1138,13 @@ desktopwin_map(ClientWin *cw)
 	
 	clientwin_render(cw);
 
+	XMapWindow(ps->dpy, cw->mini.window);
+	XRaiseWindow(ps->dpy, cw->mini.window);
+
 	if (ps->o.tooltip_show) {
 		clientwin_tooltip(cw);
 		tooltip_handle(cw->tooltip);
 	}
-
-	XMapWindow(ps->dpy, cw->mini.window);
-	XRaiseWindow(ps->dpy, cw->mini.window);
 }
 
 static bool
